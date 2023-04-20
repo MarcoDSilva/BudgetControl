8d9ed3ca2b8ce959bf7525b94070afae3978639a
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2023-04-20 23:33:07 +0100

    Adding Presentation to the project.

--- Finance.CLI/Program.cs
+++ Finance.CLI/Program.cs
@@ -1,25 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Finance.Domain.Entities;
+﻿using Finance.Domain.Entities;
+using Finance.Presentation.UI.Components;
 using Spectre.Console;
 
-var layout = new Layout("Main")
-				.SplitColumns(
-					new Layout("Left"),
-					new Layout("Right")
-						.SplitRows(
-							new Layout("Top"),
-							new Layout("Bottom")
-					)
-				);
+MainMenu main = new MainMenu();
 
-layout["Left"]
-	.Update(new Panel(Align.Center(
-			new Markup("Hello matey"),
-			VerticalAlignment.Middle))
-	.Expand());
+string selected = main.Login();
 
-layout["Left"].Ratio(2);
-layout["Right"].Ratio(2);
-layout["Main"].Ratio(5);
-
-AnsiConsole.Write(layout);
+AnsiConsole.MarkupLine($"[green]{selected}[/] was selected!");
---
+++ Finance.Presentation/UI/Components/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+
+namespace Finance.Presentation.UI.Components;
+
+public class MainMenu
+{
+	public MainMenu()
+	{
+	}
+
+	public string Login()
+	{
+		string selection = AnsiConsole.Prompt(
+				new SelectionPrompt<string>()
+				.Title("Welcome To BudgetControl!")
+				.PageSize(6)
+				.AddChoices(new[]
+				{
+					"Check Overall", "Expenses", "Income", "Investments", "Categories", "SubCategories"
+				})
+			);
+
+		return selection;
+	}
+}
